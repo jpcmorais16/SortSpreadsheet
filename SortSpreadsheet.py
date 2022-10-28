b81e3ee4cf5092fc1a4122349feb2efa0c699457
João Pedro Carolino Morais <96194625+jpcmorais16@users.noreply.github.com>
2022-10-27 22:59:44 -0300

    First

---
+++ SortSpreadsheet.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import math
+
+
+name = input('File Name: ')
+page = input('Page: ')
+columnName = input('Column to base the sorting on: ')
+
+df = pd.read_excel(name, page)
+
+column = df[columnName]
+
+functionName = input('Function to sort(none, squared, sine, cossine): ')
+def f(functionName):
+    if(functionName == 'sine'):
+        return math.sin
+    if(functionName == 'cossine'):
+        return math.cos
+    if(functionName == 'square'):
+        return lambda x: x*x
+    if(functionName == 'none'):
+        return lambda x: x
+
+    return lambda x: x
+
+
+result = df.sort_values(by=columnName)
+
+nomePlanilhaSaida = input('Exit File Name: ')
+nomePaginaPlanilha = input('Page: ')
+writer = pd.ExcelWriter(nomePlanilhaSaida, engine='xlsxwriter')
+result.to_excel(writer, sheet_name= nomePaginaPlanilha)
+writer.close()
+
+
+
